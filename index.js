7d219f70f4e12715ef77ad1249139a0903bc9372
AbdulRehmanAarmaan <abdulrehmanaarmaan@gmail.com>
2025-11-13 21:48:13 +0600

    removed a part of commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World!')
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n8udp2w.mongodb.net/?appName=Cluster0`;
@@ -183,7 +183,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        // await client.close();
+        // await client.close()
     }
 }
 run().catch(console.dir);
